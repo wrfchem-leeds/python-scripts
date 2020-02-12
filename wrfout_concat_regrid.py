ea63b2766756052b5234c03ab2ebdc5b8fbd3f00
Luke Conibear <l.a.conibear@leeds.ac.uk>
2020-02-12 19:30:13 +0000

    added aerosols under 2.5um

--- wrfout_concat_regrid.py
+++ wrfout_concat_regrid.py
@@ -6,34 +6,41 @@ import glob
 import xesmf as xe
 import numpy as np
 
-year   = '2015' 
-month  = '08'
+year   = '2015'
+month  = '01'
 res    = 0.25
 domains = ['1', '2']
 variables = ['PM2_5_DRY', 'o3', 'AOD550_sfc']
+aerosols = ['bc', 'oc', 'nh4', 'so4', 'no3', 'asoaX', 'bsoaX', 'oin']
+variables.extend(aerosols)
 surface_only = 'yes'
 regrid = 'yes'
 
 for domain in domains:
-    path = os.getcwd() # run from wrf output/base folder
+    path = os.getcwd()
     filelist = []
     filelist.extend(glob.glob(path + '/wrfout_d0' + domain + '_' + year + '-'+ month + '*'))
     filelist = sorted(filelist)
     for variable in variables:
-        # concatenate
         with salem.open_mf_wrf_dataset(filelist, chunks={'Time': -1}) as ds:
-            if (surface_only == 'yes') and (variable == 'PM2_5_DRY') or (variable == 'o3'):
+            if variable in aerosols:
+                wrf_a01 = ds[variable + '_a01'].isel(bottom_top=0)
+                wrf_a02 = ds[variable + '_a02'].isel(bottom_top=0)
+                wrf_a03 = ds[variable + '_a03'].isel(bottom_top=0)
+                wrf = wrf_a01 + wrf_a02 + wrf_a03
+            elif (surface_only == 'yes') and (variable == 'PM2_5_DRY') or (variable == 'o3'):
                 wrf = ds[variable].isel(bottom_top=0)
             else:
                 wrf = ds[variable]
 
-        # regrid
         if regrid == 'yes':
             ds_out = xr.Dataset({'lat': (['lat'], np.arange(-60, 85, 0.25)), 'lon': (['lon'], np.arange(-180, 180, 0.25)),})
             regridder = xe.Regridder(wrf, ds_out, 'bilinear', reuse_weights=True)
             wrf_regrid = regridder(wrf)
 
-        wrf_regrid.to_netcdf(path + '/wrfout_d0' + domain + '_global_'+ str(res) +'deg_' + year + '-' + month + '_' + variable + '.nc')
+        if variable in aerosols:
+            wrf_regrid.to_netcdf(path + '/wrfout_d0' + domain + '_global_'+ str(res) +'deg_' + year + '-' + month + '_' + variable + '_2p5.nc')
+        else:
+            wrf_regrid.to_netcdf(path + '/wrfout_d0' + domain + '_global_'+ str(res) +'deg_' + year + '-' + month + '_' + variable + '.nc')
 
     regridder.clean_weight_file()
-
